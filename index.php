eafc8734f1cf5d37b6cbb62979a9985f1757119c
Till Helge Helwig <thh@tshw.de>
2015-02-12 02:38:11 +0100

    Minor usability improvement for the setup phase.

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ define('IN_LAZY_CMS', true);
 
 define('ROOT_DIR', dirname(__FILE__));
 
+if (!file_exists('config.inc.php')) {
+    die("You need to create the config file at config.inc.php first.<br /><br />
+        Just rename config-sample.inc.php and customize the settings in there as necessary.");
+}
 require('config.inc.php');
 require('classes/autoload.inc.php');
 
