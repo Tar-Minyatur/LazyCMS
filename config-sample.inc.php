417d66cde223a8de9abaa4959d74b6b989690657
Till Helge Helwig <thh@tshw.de>
2015-02-12 02:37:24 +0100

    Some housekeeping to make the repository a little bit clearer. - Moved example page into an appropriate directory - Replace the actual config with a sample config, so pulling doesn't overwrite a custom one

--- config-sample.inc.php
+++ config-sample.inc.php
@@ -7,7 +7,7 @@ $lazyConfig = new stdClass;
 
 /**
  * Admin Password.
- * Use a SHA1 hash here!
+ * Use a SHA1 hash here! (Default password is "admin".)
  * To create a new one, you can use $ php -r 'echo sha1("password");'
  */
 $lazyConfig->adminPassword = 'd033e22ae348aeb5660fc2140aec35850c4da997';
@@ -15,18 +15,18 @@ $lazyConfig->adminPassword = 'd033e22ae348aeb5660fc2140aec35850c4da997';
 /**
  * The JSON file with text labels to display and save to
  */
-$lazyConfig->dataFile = ROOT_DIR . DIRECTORY_SEPARATOR . 'text_labels.json';
+$lazyConfig->dataFile = ROOT_DIR . '/example/data/text_labels.json';
 
 /**
  * Backup folder.
  * In this directoy old versions of the data file will be kept.
  */
-$lazyConfig->backupDir = ROOT_DIR . DIRECTORY_SEPARATOR . 'backup';
+$lazyConfig->backupDir = ROOT_DIR . '/backup';
 
 /**
  * Homepage URL.
  */
-$lazyConfig->homepageURL = dirname($_SERVER['PHP_SELF']) . DIRECTORY_SEPARATOR . 'output';
+$lazyConfig->homepageURL = dirname($_SERVER['PHP_SELF']) . '/example/output';
 
 /**
  * Code Generator File Mapping.
@@ -35,19 +35,24 @@ $lazyConfig->homepageURL = dirname($_SERVER['PHP_SELF']) . DIRECTORY_SEPARATOR .
  * The paths need to be relative to the root dir of LazyCMS.
  */
 $lazyConfig->fileMapping = array(
-    'input/index.html' => 'output/index.html',
-    'input/page2.html' => 'output/page2.html'
+    'example/input/index.html' => 'example/output/index.html',
+    'example/input/page2.html' => 'example/output/page2.html'
 );
 
 /**
  * Text Label Delimiter.
- * This will be prepended and appended to all text labels.
- * Default: !
- * Example: !my_textlabel!
+ * These will be prepended /appended to all text labels.
+ * Default: {{ and }}
+ * Example: {{my_textlabel}}
  */
 $lazyConfig->labelDelimiterLeft = '{{';
 $lazyConfig->labelDelimiterRight = '}}';
 
+/**
+ * Label Extractor RegEx.
+ * This is used to find labels in source files.
+ * There should (theoretically) be no reason to change this.
+ */
 $lazyConfig->labelRegEx = sprintf('/%s(?<label>[^%s]+)%s/U', 
                                   preg_quote($lazyConfig->labelDelimiterLeft),
                                   preg_quote($lazyConfig->labelDelimiterRight),
