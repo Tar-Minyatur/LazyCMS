74cf915038329bb74f7455a6937a2c14bd59f158
Till Helge Helwig <thh@tshw.de>
2015-02-21 13:07:07 +0100

    Minor change to Grunt build.

--- grunt/csslint.js
+++ grunt/csslint.js
@@ -2,7 +2,7 @@ module.exports = function (grunt)  {
     return {
 
         cms_after_sass: {
-            src: ['dist/assets/css/*.css']
+            src: ['src/assets/css/*.css']
         },
         example: {
             src: ['example/input/**.css']
