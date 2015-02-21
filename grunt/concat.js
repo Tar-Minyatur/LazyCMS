ccbfc595869822b91686f68208e1fd74c25eb63a
Till Helge Helwig <thh@tshw.de>
2015-02-21 18:08:30 +0100

    Grunt concat task should not concat the concatenated file. ;)

--- grunt/concat.js
+++ grunt/concat.js
@@ -2,11 +2,11 @@ module.exports = function (grunt) {
     return {
 
         dist: {
-            src: ['src/assets/js/*.js'],
+            src: ['src/assets/js/*.js', '!src/assets/js/<%= package.name %>.js'],
             dest: 'dist/assets/js/<%= package.name %>.js'
         },
         build: {
-            src: ['src/assets/js/*.js'],
+            src: ['src/assets/js/*.js', '!src/assets/js/<%= package.name %>.js'],
             dest: 'src/assets/js/<%= package.name %>.js'
         }
 
